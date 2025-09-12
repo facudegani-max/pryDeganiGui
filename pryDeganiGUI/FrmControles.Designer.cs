e203dbbe13f4c71bea29e1599a281da84c2b4b3f
facudegani-max <facudegani@gmail.com>
2025-09-12 12:19:48 -0300

    Agregar archivos de proyecto.

---
+++ pryDeganiGUI/FrmControles.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace pryDeganiGui
+{
+    partial class FrmControles
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnBotonera = new Button();
+            SuspendLayout();
+            // 
+            // btnBotonera
+            // 
+            btnBotonera.Location = new Point(41, 36);
+            btnBotonera.Name = "btnBotonera";
+            btnBotonera.Size = new Size(118, 23);
+            btnBotonera.TabIndex = 0;
+            btnBotonera.Text = "Abrir botonera";
+            btnBotonera.UseVisualStyleBackColor = true;
+            // 
+            // FrmControles
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 461);
+            Controls.Add(btnBotonera);
+            Name = "FrmControles";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Control de interfaz grafica";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnBotonera;
+    }
+}
---
+++ pryDeganiGUI/FrmControles.cs
@@ -0,0 +1,10 @@
+namespace pryDeganiGui
+{
+    public partial class FrmControles : Form
+    {
+        public FrmControles()
+        {
+            InitializeComponent();
+        }
+    }
+}
